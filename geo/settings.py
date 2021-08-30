9ff13ecc0743e5fdb7cdf4e8289e8e2eeb0039c9
unknown <sebastian.konicz@gmail.com>
2021-08-30 21:24:15 +0200

    Changes STATICFILES_DIRS

--- geo/settings.py
+++ geo/settings.py
@@ -125,9 +125,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
