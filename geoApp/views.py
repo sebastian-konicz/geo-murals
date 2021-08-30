ab3590d168c64055cb31a78438c732ae15a62976
unknown <sebastian.konicz@gmail.com>
2021-08-30 22:14:07 +0200

    Changes in view - removed layers

--- geoApp/views.py
+++ geoApp/views.py
@@ -13,18 +13,6 @@ def home(request):
 
     m = map_graph
 
-    # m = folium.Map(location=[-16.22, -71.59], zoom_start=10)
-
-    ## style
-    style_basin = {'fillColor': '#228B22', 'color': '#228B22'}
-    style_rivers = {'color': 'blue'}
-    ## adding to view
-
-    # folium.GeoJson(os.path.join(shp_dir, 'basin.geojson'), name='basin',
-    #                style_function=lambda x: style_basin).add_to(m)
-    # folium.GeoJson(os.path.join(shp_dir, 'rivers.geojson'), name='rivers',
-    #                style_function=lambda x: style_rivers).add_to(m)
-
     folium.LayerControl().add_to(m)
     ## exporting
     m = m._repr_html_()
