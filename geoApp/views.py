b3c29f23418a9b8fadfc274fe20d2040d95b5e7c
unknown <sebastian.konicz@gmail.com>
2024-07-11 20:52:56 +0200

    save commit

--- geoApp/views.py
+++ geoApp/views.py
@@ -14,7 +14,7 @@ def home(request):
     murals = Mural.objects.all()
 
     # folium
-    map = folium.Map(location=[52.145259, 21.051619], zoom_start=13)
+    map = folium.Map(location=[52.145259, 21.051619], zoom_start=13, height='100%')
 
     folium.LayerControl().add_to(map)
 
@@ -23,23 +23,36 @@ def home(request):
     # m.add(f)
 
     for mural in murals:
+        # variables from mural object
         coordinates = (mural.lat, mural.lon)
         tooltip = mural.title
         photo_url = mural.photo.url
 
-        # encoded = base64.b64encode(open(mural.photo, 'rb').read())
-        # html = '<img src="data:image/png;base64,{}">'.format
-        # iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
-
-
-        html = f'<img src="{photo_url}" height="300" width="400">'
+        html = f'<div id="popup"><img calss="img-responsive" src="{photo_url}"></div>'
 
         popup = folium.Popup(html=html, max_width=400)
+
+        popup_style = """
+            #popup {
+                max-width: 200px;
+                max-height: 200px;
+                overflow: hidden;
+                }
+            #popup img { 
+                width: 100%;
+                height: auto;
+                }
+            """
+
+        # marker icon
+        # icon = folium.Icon(color="red", icon='brush', prefix='fa')
+        icon = folium.Icon(icon='glyphicon-picture')
 
         # adding to view
         folium.Marker(location=coordinates,
                       tooltip=tooltip,
-                      popup=popup).add_to(map)
+                      popup=popup,
+                      icon=icon).add_to(map)
 
     ## exporting
     # making map able to render
@@ -53,3 +66,10 @@ def home(request):
 def mural_detail(request, pk):
     mural = get_object_or_404(Mural, pk=pk)
     return render(request, 'geoApp/mural_detail.html', {'mural': mural})
+
+# encoded = base64.b64encode(open(mural.photo, 'rb').read())
+# html = '<img src="data:image/png;base64,{}">'.format
+# iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
+
+# height = "300"
+# width = "400"
